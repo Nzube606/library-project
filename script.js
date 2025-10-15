fc63c934fa0f59456f4e20e5deddd63f37766188
Ilodigwe Nzube <ilodigwenzube142@gmail.com>
2025-10-15 05:59:27 +0100

    Done with the functionalities

--- script.js
+++ script.js
@@ -1,134 +1,98 @@
 const myLibrary = [];
 
 // book constructor
-function Book(title, author, pages, read ) {
+function Book(title, author, pages, read) {
   this.title = title,
   this.author = author,
   this.pages = pages;
   this.read = read;
+  this.id = crypto.randomUUID(); // unique ID for each object created with this constructor. Read up data-attributes
 }
 
-// Add method to the book prototype so other object instances can have access
-Book.prototype.info = function () {
-  let bookProp = `Title:    ${this.title} \n
-  Author:    ${this.author} \n
-  Number of pages:    ${this.pages} \n
-  Read status:    ${this.read}.  ` ;
-  // this.id;
-  let bookId = crypto.randomUUID(); // unique ID for each object created with this constructor. Read up data-attributes
-    return {bookProp, bookId};  //returns as an object
-    };
-    // const bookInfo = Book.info();
-
-function addBookToLibrary(title, author, pages, read) {
-  // take params, create a book then store it in the array
-  let book = new Book(title, author, pages, read);
-  // console.log(book.id)
-  // let bookInfo = book.info();
-  myLibrary.push(book.info()); // push the new book object to the library array with info method
+  function addBookToLibrary(title, author, pages, read) {
+    // take params, create a book then store it in the array
+    const book = new Book(title, author, pages, read);
+myLibrary.push(book); // push the new book object to the library array with info method
 }
 
-// addBookToLibrary('Rich Dad', "Rowling", 499, 'Yes');
-// addBookToLibrary('Poor Dad', "J.K", 500, 'No');
-// console.log(myLibrary);
-
-// displayLibrary()  
-const library = document.querySelector('.library');
-const form = document.querySelector('#form');
-const dialog = document.querySelector('dialog')
-const libraryCard = document.querySelector('.library-card');
-const addBookToLib = document.querySelector('.addBookToLib');
-const cancelBtn = dialog.querySelector('.cancel');
-const addBookBtn = dialog.querySelector('.add');
-const titleBox = dialog.querySelector('input[name="title"]');
-const authorBox = dialog.querySelector('input[name="author"]');
-const pageBox = dialog.querySelector('input[name="pages"]');
-const radios = document.querySelectorAll('input[name="read"]');
-const removeBtns = document.querySelectorAll('.removeBtn')
-// const readBox = document.querySelector('input[name="read"]');
-// libraryCard.style.display = 'hidden';
-// removeBtns.forEach(removeBtn => {
-  //   removeBtn.addEventListener('click', () => {
-    //     console.log('button-press')
-    //     library.removeChild(CardDiv);
-    //   })
-    // })
-    radios.forEach(radio => {
-      radio.addEventListener('click', () => {
-        // if(radio.checked){
-         readBox = radio.value;
-        // }
-        // else {
-        //   readBox = 'None selected';
-        // }
-      })
-    });
-
-    function displayLibrary(){
-        library.textContent = "" // clear the library when called so as to not repeat contents 
-        myLibrary.forEach(book => {
-          const cardDiv = document.createElement('div');
-          cardDiv.classList.add('libraryCard');
-          cardDiv.textContent = book.bookProp;
-          // create a button to toggle read status
-          const toggleBtn = document.createElement('button');
-          const readStatus = book.bookProp.read;
-          console.log(readStatus);
-          toggleBtn.textContent = `Read: ${book.t}`;
-          toggleBtn.classList.add('toggleBtn');
-          // add event listener to toggle when pressed
-          toggleBtn.addEventListener('click', () => {
-            // let readStatusPosition = book.bookProp.this.;
-            this.read === "Yes" ? 'No' : 'Yes';
-            this.read === 'No' ? 'Yes' : "No" ;
-          })
-          const removeBtn = document.createElement('button'); // create a remove button
-          removeBtn.classList.add('removeBtn');
-          removeBtn.textContent = "Remove";
-          // loop through the array of objects, myLibrary and get the index
-          const index = myLibrary.findIndex(book => book.bookId); 
-          removeBtn.addEventListener('click', () => {
-            library.removeChild(cardDiv); // make the card to disappear from screen
-            myLibrary.splice(index, 1); // delete the card from the array
-            // library.textContent = "";
+            
+    function displayLibrary(){  // function to create the contents of each book and display it (the library). 
+      const library = document.querySelector('.library');
+      library.textContent = "" // clear the library when called so as to not repeat contents 
+      myLibrary.forEach((book) => {
+        const cardDiv = document.createElement('div'); // Div to serve as card for each book
+        cardDiv.classList.add('libraryCard');
+        
+        const title = document.createElement('h3');
+        title.textContent = book.title;
     
-          });
-          cardDiv.appendChild(toggleBtn);
-          cardDiv.appendChild(removeBtn);
-          library.appendChild(cardDiv);
-        })
-    }
+        const author = document.createElement('p');
+        author.textContent =`By: ${book.author}` 
+        
+        const pages = document.createElement('p');
+        pages.textContent =`${book.pages} pages`
+    
+    const read = document.createElement('p');
+    read.textContent =`Read: ${book.read}`
+    
+    const toggleBtn = document.createElement('button');  // create a button to toggle read status
+    toggleBtn.classList.add('toggleBtn');
+    toggleBtn.textContent = book.read === 'Yes' ? toggleBtn.textContent = 'Mark unread' : toggleBtn.textContent = 'Mark read';   
+   // the above code changes the toggle button content to 'mark read' when the 
+   // read status is 'No' or 'None selected' and to "mark unread" when the read status is 'yes' 
 
-    // const bookTitle = titleBox.textContent;
-// const bookAuthor = authorBox.textContent;
-// const bookPages = pageBox.textContent;
-// const readBook = readBox.textContent;
-// addBookToLib.addEventListener('click', displayLibrary);
-addBookToLib.addEventListener('click', () => {
-  dialog.showModal();
-  // console.log("hi")
-  
-})
-cancelBtn.addEventListener('click', () => {
-  dialog.close();
-  form.reset() ;
-})
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-})
-addBookBtn.addEventListener('click', () => {
-  readBox = "None selected";
-  radios.forEach(radio => {
-    if(radio.checked) {
-      readBox = radio.value;
-    }
+    toggleBtn.addEventListener('click', () => {  // add event listener to toggle when pressed
+      book.read = book.read === "Yes" ? 'No' : 'Yes'; // when the button is pressed it changes to 'No' or 'Yes' depending on the value at the time
+      displayLibrary(); // calls the function again to effect change.
+    })
+    const removeBtn = document.createElement('button'); // create a remove button.
+    removeBtn.classList.add('removeBtn');
+    removeBtn.textContent = "Remove";
+    
+    const id = book.id; // assign each book id a particular variable(id) so it doesn't change.
+    removeBtn.addEventListener('click', () => { 
+      let index = myLibrary.findIndex(book => book.id == id) //find the index of the id
+      myLibrary.splice(index, 1); // delete the card from the array
+      displayLibrary(); // call the function again to effect change. 
   });
-  addBookToLibrary(titleBox.value, authorBox.value, pageBox.value, readBox);
-  // console.log(myLibrary)
-  // addBookToLibrary(bookTitle, bookAuthor, bookPages, readBook);
+  cardDiv.append(title, author, pages, read, toggleBtn, removeBtn); // append all the created elements to the parent(Card) 
+  library.appendChild(cardDiv); // append the card to the screen.
+})
+}
 
-  displayLibrary();
-  dialog.close();
-  form.reset(); // resets all the input and radio values
+      const dialog = document.querySelector('dialog');
+      const addBookToLib = document.querySelector('.addBookToLib');
+      addBookToLib.addEventListener('click', () => { // event listener to show dialog modal
+        dialog.showModal();
+      })
+
+      const cancelBtn = dialog.querySelector('.cancel');
+      cancelBtn.addEventListener('click', () => { // event listener to close dialog modal
+        dialog.close();
+        form.reset() ;  // resets all the input and radio values when cancelled.
+      })
+      
+      const form = document.querySelector('#form');
+      form.addEventListener('submit', (event) => { // this is to prevent the default form submission.
+        event.preventDefault()
+      })
+
+      const addBookBtn = dialog.querySelector('.add');
+      addBookBtn.addEventListener('click', () => { // event listener for the add book button
+        const title = dialog.querySelector('#title').value;
+        const author = dialog.querySelector('#author').value;
+        const page = dialog.querySelector('#pages').value;
+        const read = document.querySelector('input[name="read"]:checked');
+
+        if(read){ // display the selected value
+          addBookToLibrary(title, author, page, read.value);
+        }
+        else { // display when no value is selected
+          const selectedValue = "None selected";
+          addBookToLibrary(title, author, page, selectedValue);
+        }
+        
+        displayLibrary(); // call the library function to show the book(s) when book(s) is added.
+        dialog.close();
+        form.reset(); // resets all the input and radio values when a book is added to start form input afresh.
 })
